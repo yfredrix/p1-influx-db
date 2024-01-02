05f758a3375dc3916e1056fcade4d55494b9b4d9
yfredrix <skier@live.nl>
2024-01-03 00:29:13 +0100

    Debug

--- p1_influx_db/main.py
+++ p1_influx_db/main.py
@@ -1,5 +1,19 @@
 from dsmr_parser import telegram_specifications
 from dsmr_parser.clients import SerialReader, SERIAL_SETTINGS_V5
+import influxdb_client
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+# bucket = "<my-bucket>"
+# org = "<my-org>"
+# token = "<my-token>"
+# # Store the URL of your InfluxDB instance
+# url=
+
+# client = influxdb_client.InfluxDBClient(
+#    url=url,
+#    token=token,
+#    org=org
+# )
 
 
 serial_reader = SerialReader(
@@ -13,7 +27,10 @@ for telegram in serial_reader.read_as_object():
     for attr, value in telegram:
         print(attr)
         if attr == 'POWER_EVENT_FAILURE_LOG':
-            continue
+            print("POWER_EVENT_FAILURE_LOG")
+            print(value)
+            print(value.to_json())
+            print("DONE")
         if isinstance(value, list):
             for sub_value in value:
                 print(sub_value.to_json())
