81f52963418081be39be64993c5e4769db418f17
yfredrix <skier@live.nl>
2024-01-03 00:31:40 +0100

    Refactor code to parse and print JSON data from telegram

--- p1_influx_db/main.py
+++ p1_influx_db/main.py
@@ -3,6 +3,8 @@ from dsmr_parser.clients import SerialReader, SERIAL_SETTINGS_V5
 import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 
+import json
+
 # bucket = "<my-bucket>"
 # org = "<my-org>"
 # token = "<my-token>"
@@ -24,16 +26,8 @@ serial_reader = SerialReader(
 
 
 for telegram in serial_reader.read_as_object():
-    for attr, value in telegram:
-        print(attr)
-        if attr == 'POWER_EVENT_FAILURE_LOG':
-            print("POWER_EVENT_FAILURE_LOG")
-            print(value)
-            print(value.to_json())
-            print("DONE")
-        if isinstance(value, list):
-            for sub_value in value:
-                print(sub_value.to_json())
-        else:
-            print(value.to_json())
+    parsed_telegram = json.loads(telegram.to_json())
+    for k, v in parsed_telegram.items():
+        print(k)
+        print(v)
     break
