850b3e9c72b3ec41cdfa8b83db7abb8b498abd04
yfredrix <skier@live.nl>
2024-01-02 23:26:58 +0100

    Add P1 serial parsing to Influx DB

---
+++ p1-influx-db/main.py
@@ -0,0 +1,12 @@
+from dsmr_parser import telegram_specifications
+from dsmr_parser.clients import SerialReader, SERIAL_SETTINGS_V5
+
+
+serial_reader = SerialReader(
+    device='/dev/ttyUSB0',
+    serial_settings=SERIAL_SETTINGS_V5,
+    telegram_specification=telegram_specifications.V5
+)
+
+for telegram in serial_reader.read_as_object():
+    print(telegram)  # see 'Telegram object' docs below
