d98fc0393f0bf3a7f6e642e47b2b84ec89ee26b0
yfredrix <skier@live.nl>
2024-07-20 18:58:19 +0200

    fix: Refactor argument parsing in __main__.py
    The code changes refactor the argument parsing in the __main__.py file. The unnecessary newline and indentation in the argument definition are removed, resulting in cleaner and more concise code.

--- p1_influx_db/__main__.py
+++ p1_influx_db/__main__.py
@@ -3,15 +3,17 @@ import argparse
 from p1_influx_db import method
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--config", help="config file", default="./p1_influx_db/example_config.toml")
+parser.add_argument(
+    "--config", help="config file", default="./p1_influx_db/example_config.toml"
+)
 args = parser.parse_args()
 if method == "mqtt":
-    from p1_influx_db import mqttmain
+    from p1_influx_db.mqttmain import mqttmain
 
     mqttmain(args.config)
 elif method == "http":
     import asyncio
-    from p1_influx_db import httpmain as mainhttp
+    from p1_influx_db.httpmain import httpmain as mainhttp
 
     asyncio.run(mainhttp(args.config))
 else:
