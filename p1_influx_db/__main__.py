a33d807eeae4684570661a6865330c66940d8443
yfredrix <skier@live.nl>
2024-07-20 18:51:40 +0200

    fix: Remove unused imports and modules in __main__.py
    The code changes remove unused imports and modules in the __main__.py file. This refactor improves the code's cleanliness and reduces unnecessary dependencies.

--- p1_influx_db/__main__.py
+++ p1_influx_db/__main__.py
@@ -1,14 +1,19 @@
 import argparse
 
-from p1_influx_db import method, httpmain as mainhttp, mqttmain
+from p1_influx_db import method
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--config", help="config file", default="./p1_influx_db/example_config.toml")
+parser.add_argument(
+    "--config", help="config file", default="./p1_influx_db/example_config.toml"
+)
 args = parser.parse_args()
 if method == "mqtt":
+    from p1_influx_db import mqttmain
+
     mqttmain(args.config)
 elif method == "http":
     import asyncio
+    from p1_influx_db import httpmain as mainhttp
 
     asyncio.run(mainhttp(args.config))
 else:
